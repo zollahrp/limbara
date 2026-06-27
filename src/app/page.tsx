16508e3fe312eda9acb6c413ec834dfa078e4a01
rivalfitrah <rivalfitrah9@gmail.com>
2026-06-27 10:03:23 +0700

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import AIFlowSection from "@/components/home/AIFlowSection";
 import CTASection from "@/components/home/CTASection";
 
 import { SCROLL_THRESHOLDS } from "@/constants/HomeData";
+import ChatbotWidget from "@/components/ Chatbotwidget";
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -39,6 +40,7 @@ export default function Home() {
       <RecyclableSection />
       <AIFlowSection />
       <CTASection />
+      <ChatbotWidget />
       
       <div className="snap-start">
         <Footer />
